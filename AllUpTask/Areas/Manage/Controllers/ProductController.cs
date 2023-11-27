b18f9ef6c742a43d752e1fa7b538457c68da5cb2
Tenzile04 <tenzileabdiyeva2004@gmail.com>
2023-11-28 03:25:59 +0400

    first commit

---
+++ AllUpTask/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,175 @@
+﻿using AllUpTask.DAL;
+using AllUpTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllUpTask.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _context.Products.ToList();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+           
+            ViewBag.Brands = _context.Brands.ToList();
+            ViewBag.Tags = _context.Tags.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            ViewBag.Brands = _context.Brands.ToList();
+            ViewBag.Tags = _context.Tags.ToList();
+            if (!ModelState.IsValid) return View(product);
+
+            if (!_context.Brands.Any(x => x.Id == product.BrandId))
+            {
+                ModelState.AddModelError("BrandId", "Brand Not found");
+                return View();
+            }
+           
+
+            bool check = false;
+            if (product.TagIds != null)
+            {
+                foreach (var tagId in product.TagIds)
+                {
+                    if (!_context.Tags.Any(x => x.Id == tagId))
+                    {
+                        check = true;
+                        break;
+                    }
+                }
+            }
+            if (check)
+            {
+                ModelState.AddModelError("TagId", "Tag not found");
+                return View();
+            }
+            else
+            {
+                if (product.TagIds != null)
+                {
+                    foreach (var tagId in product.TagIds)
+                    {
+                        ProductTag productTag = new ProductTag
+                        {
+                            Product = product,
+                            TagId = tagId,
+
+                        };
+                        _context.ProductTags.Add(productTag);
+                    }
+                }
+            }
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+
+            ViewBag.Brands = _context.Brands.ToList();
+            ViewBag.Tags = _context.Tags.ToList();
+            Product existProduct = _context.Products.Include(x => x.ProductTags).FirstOrDefault(x => x.Id == id);
+            if (existProduct == null)
+            {
+                return NotFound();
+            };
+            existProduct.TagIds = existProduct.ProductTags.Select(bt => bt.TagId).ToList();
+
+            return View(existProduct);
+        }
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            ViewBag.Brands = _context.Brands.ToList();
+            ViewBag.Tags = _context.Tags.ToList();
+
+            if (!ModelState.IsValid) return View();
+
+            Product existProduct = _context.Products.FirstOrDefault(b => b.Id == product.Id);
+            if (existProduct == null) return NotFound();
+            if (!_context.Brands.Any(g => g.Id == product.BrandId))
+            {
+                ModelState.AddModelError("BrandId", "Brand not found!");
+                return View();
+            }
+
+            var existproduct = _context.Products.Include(x => x.ProductTags).FirstOrDefault(x => x.Id == product.Id);
+            if (existproduct == null)
+            {
+                return NotFound();
+            }
+
+
+            existProduct.ProductTags.RemoveAll(bt => !product.TagIds.Contains(bt.TagId));
+
+            foreach (var tagId in product.TagIds.Where(t => !existProduct.ProductTags.Any(bt => bt.TagId == t)))
+            {
+                ProductTag productTag = new ProductTag
+                {
+                    TagId = tagId
+                };
+                existProduct.ProductTags.Add(productTag);
+            }
+            existProduct.Name = product.Name;
+            existProduct.Desc = product.Desc;
+            existProduct.SalePrice = product.SalePrice;
+            existProduct.CostPrice = product.CostPrice;
+            existProduct.DiscountPercent = product.DiscountPercent;
+            existProduct.IsAvailable = product.IsAvailable;
+            existProduct.Tax = product.Tax;
+            existProduct.Code = product.Code;
+            existProduct.BrandId = product.BrandId;
+            _context.SaveChanges();
+            return RedirectToAction("index");
+        }
+        public IActionResult Delete(int id)
+        {
+
+
+            ViewBag.Brands = _context.Brands.ToList();
+            ViewBag.Tags = _context.Tags.ToList();
+            if (id == null) return NotFound();
+
+            Product existProduct = _context.Products.FirstOrDefault(b => b.Id == id);
+            if (existProduct == null) return NotFound();
+
+
+            return View(existProduct);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            ViewBag.Brands = _context.Brands.ToList();
+            ViewBag.Tags = _context.Tags.ToList();
+
+            Product existProduct = _context.Products.FirstOrDefault(b => b.Id == product.Id);
+            if (existProduct == null) return NotFound();
+
+            _context.Products.Remove(existProduct);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ AllUpTask/Migrations/20231127065334_products.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AllUpTask.Migrations
+{
+    public partial class products : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Desc = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tax = table.Column<double>(type: "float", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    CostPrice = table.Column<double>(type: "float", nullable: false),
+                    SalePrice = table.Column<double>(type: "float", nullable: false),
+                    DiscountPercent = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductTags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductTags_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTags_ProductId",
+                table: "ProductTags",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTags_TagId",
+                table: "ProductTags",
+                column: "TagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductTags");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
